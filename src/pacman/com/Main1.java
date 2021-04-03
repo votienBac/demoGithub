b4b888bd0c8e5f3c64d972f4ad6f6a4c68f54fe6
votienBac <69105404+votienBac@users.noreply.github.com>
2021-04-03 10:23:41 +0700

    Update Main1.java

--- src/pacman/com/Main1.java
+++ src/pacman/com/Main1.java
@@ -3,5 +3,6 @@ package pacman.com;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("day la ban chinh sua");
+        System.out.println("chinh sua moi");
     }
 }
